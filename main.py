90a317c5fb9a399f321f5774ea6fab85236b6a8e
phoenixp123 <68199764+phoenixp123@users.noreply.github.com>
2020-10-01 01:06:23 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-# Imports the Google Cloud client library
+# Imports the Google Cloud client library + other necessary libraries
 from google.cloud import language
 from google.cloud.language import types
 from google.cloud.language import enums
@@ -14,9 +14,9 @@ client = language.LanguageServiceClient.from_service_account_json \
 
 
 class TextObject:
-    def __init__(self,url,language):
+    def __init__(self,url,language): # initialize our TextObject
         self.url = url
-        self.language = language
+        self.language = language # support will be added later for this feature -> multilingual sentiment analysis
 
     def extractText(self):
         r1 = requests.get(self.url)
@@ -32,7 +32,7 @@ class TextObject:
 
     def findSentiment(self,df):
         dfString = [d for d in df["text"]]
-
+        # iterate through each paragraph of text
         for index,s in enumerate(dfString):
             document = types.Document(
                 content = s,
@@ -47,22 +47,34 @@ class TextObject:
 
 if __name__ == "__main__":
     urls = ["https://www.nytimes.com/2016/05/01/magazine/what-happened-to-worcester.html",
-            "https://www.economist.com/graphic-detail/2020/09/30/donald-trump-did-himself-no-favours-in-the-first"
-            "-presidential-debate ",
+            "https://clarknow.clarku.edu/2020/09/29/will-mahan-23-crafts-history-lessons-for-a-new-generation/",
             "https://www.theguardian.com/world/2020/sep/30/north-carolina-university-duke-coronavirus",
             "https://cooking.nytimes.com/recipes/1021483-salt-baked-new-potatoes-with-pink-peppercorn-butter?action"
             "=click&region=Sam%20Sifton%27s%20Suggestions&rank=1 "
             ]
-    for index,website in enumerate(urls):
+
+    plt.figure(figsize = (10,6))
+    for index,website in enumerate(urls): # iterate through the sentiment data for each webpage
         t = TextObject(website,'english')
         text = t.extractText()
         text = t.findSentiment(text)
-        #print("The average sentiment(positive/negative, +1/-1) is : {}".format(np.mean(text["score"])))
-        #print("The average magnitude(0/+inf) is : {}".format(np.mean(text["magnitude"])))
-        #print("\n")
-        plt.subplot(2,2,index+1)
-        x_vals_scores = list(range(len(text["score"])))
-        y_vals_score = text["score"]
+        print("The average sentiment(positive/negative, +1/-1) is : {}".format(np.mean(text["score"])))
+        print("The average magnitude(0/+inf) is : {}".format(np.mean(text["magnitude"])))
+        print("\n")
 
-        plt.plot(x_vals_scores, y_vals_score)
+        plt.figure(figsize = (10,6))
+        plt.ylim(-1, 8)
+
+        x_vals_scores = list(range(len(text["score"])))
+        y_vals_scores = text["score"]
+        plt.plot(x_vals_scores, y_vals_scores, label="positive/negative sentiment")
+
+        y_vals_magnitude = text["magnitude"]
+        plt.plot(x_vals_scores, y_vals_magnitude, label="magnitude of sentiment")
+
+        plt.xlabel("From beginning to end of article")
+        plt.ylabel("Magnitude & Score")
+
+        plt.legend('best', fontsize = 15)
+
         plt.show()
